405f1d3208161acfaf141f33b6181a2b945b5ed4
HowdyHowe <amiruddinstr@gmail.com>
2025-10-23 02:55:56 +0700

    Code adjustment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
-  router.push("/login")
-  return;
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
+  return null;
 }
